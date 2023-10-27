87c7e8aece70ac829ba1172afbdd3b2d09d9da87
Karolina Dabrowska <ikonygdansk@gmail.com>
2023-10-27 17:04:03 +0200

    Change 1

--- shop.py
+++ shop.py
@@ -1,16 +1,18 @@
-print("Lista zakupów")
+#Lista produktów z piekarni i warzywniaka
 shopping_list = {
     "Piekarnia": ['Chleb', 'Pączek', 'Bułki'],
     "Warzywniak": ['Marchew', 'Seler', 'Rukola']
 }
 sum_list = 0
 
+#Przeiterowanie po słowniku, oraz zmiana nazw na wielkie ltery 
 for shop, products in shopping_list.items():
     shop = shop.upper()
     products = [product.upper() for product in products]
 
     print(f"Idę do {shop}, kupuję tu następujące rzeczy: {products}.")
 
+#suma produktów
     sum_list += len(products)
 
 print(f"W sumie kupuję {sum_list} produktów.")
